7403c45359b356c37fcb3bd47b1a9d174558de0d
Hamid <hamidtahir2000@gmail.com>
2021-06-16 17:19:28 +0500

    sd

--- app.js
+++ app.js
@@ -6,13 +6,13 @@ const rightColumn = document.querySelector('.prepu-column-inner-right');
 document.querySelector('.radio-left').addEventListener('click', function(){
   link.setAttribute('href','http://tinystuds.com/cart/40185303498939:1,40605506568379:1?discount=earlybird38');
   amount.innerText = '19';
-  rightColumn.classList.toggle('selected');
-  leftColumn.classList.toggle('selected');
+  rightColumn.classList.remove('selected');
+  leftColumn.classList.add('selected');
 });
 
 document.querySelector('.radio-right').addEventListener('click', function(){
   link.setAttribute('href','http://tinystuds.com/cart/40185303498939:1,40602300022971:1?discount=earlybird38');
   amount.innerText = '23';
-  rightColumn.classList.toggle('selected');
-  leftColumn.classList.toggle('selected');
+  rightColumn.classList.add('selected');
+  leftColumn.classList.remove('selected');
 });
